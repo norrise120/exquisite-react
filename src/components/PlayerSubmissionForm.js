8b1d6446c588665596dc4904678d5e4184122427
Erica Norris <norrise120@gmail.com>
2019-06-19 16:14:58 -0700

    start of form for playersubmission

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,6 +5,25 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      adjective: "",
+      noun: "",
+      adverb: "",
+      verb: "",
+      adjective2: "",
+      noun2: ""
+    };
+  }
+  
+  onInputChange = (event) => {
+    console.log(event.target.name)
+    const updatedState = {};
+
+    const field = event.target.name;
+    const value = event.target.value;
+
+    updatedState[field] = value;
+    this.setState(updatedState);
   }
 
   render() {
@@ -16,14 +35,58 @@ class PlayerSubmissionForm extends Component {
         <form className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
-            <input
-              placeholder="hm..."
-              type="text" />
-
+            
+            The
+            <label>
+              <input
+              placeholder="Adjective"
+              type="text" 
+              value={this.state.adjective}
+              onChange={this.onInputChange}
+              name="adjective"/>
+            </label>
+            <label>
+              <input
+              placeholder="Noun"
+              type="text" 
+              value={this.state.noun}
+              onChange={this.onInputChange}
+              name="noun"/>
+            </label>
+            <label>
+              <input
+              placeholder="Adverb"
+              type="text" 
+              value={this.state.adverb}
+              onChange={this.onInputChange}
+              name="adverb"/>
+            </label>
+            <label>
+              <input
+              placeholder="Verb"
+              type="text" 
+              value={this.state.verb}
+              onChange={this.onInputChange}
+              name="verb"/>
+            </label>
+            the
+            <label>
+              <input
+              placeholder="Adjective 2"
+              type="text" 
+              value={this.state.adjective2}
+              onChange={this.onInputChange}
+              name="adjective2"/>
+            </label>
+            <label>
+              <input
+              placeholder="Noun 2"
+              type="text" 
+              value={this.state.noun2}
+              onChange={this.onInputChange}
+              name="noun2"/>
+            </label>
+            .
           </div>
 
           <div className="PlayerSubmissionForm__submit">
